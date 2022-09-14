f836b8d945511c5ed510fa580408027e2b26e93c
root <root@f8a879b84431>
2022-09-14 23:29:48 +0000

    Badger, Banana, Banana_Split, FixThePrint, HelloBob, MakeItCount

--- src/_05_for_loops/_2_badgers/Badger.java
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -1,7 +1,18 @@
 package _05_for_loops._2_badgers;
 
 public class Badger {
-}
-{
-badger
+	public static void main(String[] args) {
+		for (int verse = 0; verse < 2; verse++) {
+			for (int badger = 0; badger < 12; badger++) {
+				System.out.print("Badger, ");
+			}
+			System.out.println();
+			for (int mushroom = 0; mushroom < 2; mushroom++) {
+				System.out.print("Mushroom, ");
+
+			}
+			System.out.println();
+		}
+		System.out.println("A snake!");
+	}
 }
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,6 @@
+for (int icecream = 0; icecream < 3; icecream++){
+text("Ice Cream",50,50+icecream*10);
+}
+for (int banana = 0;  banana < 1; banana++){
+text("Banana",50,90);
+}
